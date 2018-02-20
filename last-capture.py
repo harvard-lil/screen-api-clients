b21f0530c083fd02e4dbb9444adf6759f2ac8816
Ben Steinberg <bsteinberg@law.harvard.edu>
2018-02-20 12:00:48 -0500

    Ignore errors

--- last-capture.py
+++ last-capture.py
@@ -16,13 +16,16 @@ headers = {"Authorization": "Token %s" % token}
 
 
 while True:
-    objects = get_objects(20,0)
-    for object in objects:
-        if object[-2] is not None:
-            domain = object[0].split("//")[-1].split("/")[0]
-            last = object[-2]
-            p = u"∞ Last Perma capture, of {1}, was {0}.".format(
-                humanize.naturaltime(datetime.now() - timedelta(seconds=last)), domain)
-            go(url, room, headers, p)
-            break
+    try:
+        objects = get_objects(20,0)
+        for object in objects:
+            if object[-2] is not None:
+                domain = object[0].split("//")[-1].split("/")[0]
+                last = object[-2]
+                p = u"∞ Last Perma capture, of {1}, was {0}.".format(
+                    humanize.naturaltime(datetime.now() - timedelta(seconds=last)), domain)
+                go(url, room, headers, p)
+                break
+    except:
+        pass
     sleep(60)
